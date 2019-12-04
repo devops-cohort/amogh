198890dcb059a651ec48c1addc65416800f073db
amoghvnaik <amoghvnaik@yahoo.com>
2019-12-04 09:46:32 +0000

    commit

--- bookreviews/application/forms.py
+++ bookreviews/application/forms.py
@@ -82,9 +82,15 @@ class RegisterForm(FlaskForm):
             raise ValidationError('Email is already in use!')
 
 class UpdateAccountForm(FlaskForm):
+    first_name = StringField('First Name',
+        validators=[DataRequired(), Length(min=4, max=30)])
+
+    last_name = StringField('Last Name',
+        validators=[DataRequired(), Length(min=4, max=30)])
+
     email = StringField('Email',
-        validators=[DataRequired(), Email()])                             
-    
+        validators=[DataRequired(), Email()])
+
     submit = SubmitField('Update')
 
     def validate_email(self, email):
